094337c4da80c85087632cc54776b721bd363fbd
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-03-21 00:51:24 +0100

    Work on reading JSON API

--- app/src/main/java/android/example/com/quakereport1/RetrieveDataTask.java
+++ app/src/main/java/android/example/com/quakereport1/RetrieveDataTask.java
@@ -4,12 +4,18 @@ import android.os.AsyncTask;
 import android.util.JsonReader;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -17,57 +23,126 @@ import javax.net.ssl.HttpsURLConnection;
  * Created by neo on 18/03/2018.
  */
 
-public class RetrieveDataTask extends AsyncTask<Void, String, InputStream> {
-    @Override
-    protected InputStream doInBackground(Void... params) {
-        Log.d("RetrieveDataTask", "inside RetrieveDataTask");
+public class RetrieveDataTask extends AsyncTask<Void, String, String> {
 
-        HttpsURLConnection myConnection;
-        InputStream responseBody = null;
+    /** Tag for the log messages */
+    public static final String LOG_TAG = RetrieveDataTask.class.getSimpleName();
 
-        URL usgsUrl = null;
-        try {
-            usgsUrl = new URL("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-03-17&endtime=2018-03-17&minmag=4&limit=100");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+    private String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-03-17&endtime=2018-03-17&minmag=4&limit=100";
+
+    // read the responsebody containing the JSON payload
+    // java is f* ugly! viva python
+    private String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
         }
+        Log.d(LOG_TAG, "readFromStream about to return: " + output.toString());
+        return output.toString();
+    }
 
-        String responseString = null;
-
-        // Create connection
+    // assemble the URL
+    private URL createUrl(String stringUrl) {
+        URL url = null;
         try {
-            myConnection =
-                    (HttpsURLConnection) usgsUrl.openConnection();
-            int response = -1;
-            try {
-                response = myConnection.getResponseCode();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Log.d("RetrieveDataTask", "http response: " + response);
-            if (response == HttpURLConnection.HTTP_OK) {
-//                // Success
-//                // Further processing here
-                Log.d("RetrieveDataTask", "got a 200!");
-//                Log.d("RetrieveDataTask", "myConnection: " + String.valueOf(myConnection.getContent()));
-                responseBody = myConnection.getInputStream();
-            }
+            url = new URL(stringUrl);
+        } catch (MalformedURLException exception) {
+            Log.e(LOG_TAG, "Error with creating URL", exception);
+            return null;
+        }
+        Log.d(LOG_TAG, "createUrl about to return: " + url.toString());
+        return url;
+    }
+
+    // make http request and return the response body as a string
+    private String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.connect();
+            inputStream = urlConnection.getInputStream();
+            jsonResponse = readFromStream(inputStream);
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e("TAG", "exception in makeHttpRequest " + e.getLocalizedMessage());
+            throw e;
+            // TODO: Handle the exception
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // function must handle java.io.IOException here
+                inputStream.close();
+            }
         }
-        Log.d("RetrieveDataTask", "returning responseBody: ");
-        return responseBody;
+        return jsonResponse;
+    }
+
+    private String extractFeatureFromJson(String earthquakeJSON) {
+        Log.d("extractFeatureFromJson", "earthquakeJSON: " + earthquakeJSON);
+        try {
+            JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+            JSONArray featureArray = baseJsonResponse.getJSONArray("features");
+
+            Log.d("extractFeatureFromJson", "featureArray.length(): " + featureArray.length());
+
+            // If there are results in the features array
+            if (featureArray.length() > 0) {
+                // Extract out the first feature (which is an earthquake)
+                JSONObject firstFeature = featureArray.getJSONObject(0);
+                JSONObject properties = firstFeature.getJSONObject("properties");
+
+                // Extract out the title, time, and tsunami values
+                String title = properties.getString("title");
+                long time = properties.getLong("time");
+                int tsunamiAlert = properties.getInt("tsunami");
+
+                Log.d("extractFeatureFromJson", "returning: "
+                        + title + " " + time + " " + tsunamiAlert);
+                // Create a new {@link Event} object
+                return new String(title + time + tsunamiAlert);
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+        }
+        return null;
     }
 
     @Override
-    protected void onPostExecute(InputStream inputStream) {
-        super.onPostExecute(inputStream);
+    protected String doInBackground(Void... params) {
+        Log.d("RetrieveDataTask", "inside doInBackground");
 
+        URL url = createUrl(USGS_REQUEST_URL);
+        String jsonResponse = "";
         try {
-            Log.v("onPostExecute", "I have inputStream: " + inputStream.read());
+            jsonResponse = makeHttpRequest(url);
         } catch (IOException e) {
-            e.printStackTrace();
+            // TODO Handle the IOException
         }
+
+        // Extract relevant fields from the JSON response and create an {@link Event} object
+        String earthquake = extractFeatureFromJson(jsonResponse);
+
+        Log.d("RetrieveDataTask", "earthquake: " + earthquake);
+        return earthquake;
+    }
+
+    @Override
+    protected void onPostExecute(String earthquake) {
+        super.onPostExecute(earthquake);
+
+        Log.v("onPostExecute", "I have earthquake: " + earthquake);
     }
 
     //    @Override
